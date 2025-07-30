57ce1836279d99baceb948dc186b03b696eef5d5
alain <alainbloos@gmail.com>
2025-07-30 20:17:15 -0300

    [REF] resolved pending "TODO os"s in python

--- account_batch_payment_ux/models/account_batch_payment.py
+++ account_batch_payment_ux/models/account_batch_payment.py
@@ -41,15 +41,4 @@ class AccountBatchPayment(models.Model):
         self.payment_ids.is_move_sent = False
         self.write({'state': 'draft'})
 
-    # Agregamos este código porque Odoo hizo un cambio de los depends de este método que hace que falle
-    # https://github.com/odoo/enterprise/pull/70264
-    # No falla en runbot de odoo porque el módulo de sepa direct debit tiene un método similar a este
-    # Ticket a odoo con la explicación: https://www.odoo.com/es_ES/my/tasks/4296137
-    # Una vez que lo solucionen en Odoo, deberíamos hacer un revert de este código
-    # TODO os: why there is function that call only super?
-    @api.constrains('batch_type', 'journal_id', 'payment_ids')
-    def _check_payments_constrains(self):
-        if self.env.company.country_code == 'AR':
-            super(AccountBatchPayment, self)._check_payments_constrains()
-        else:
-            return super()._check_payments_constrains()
+
